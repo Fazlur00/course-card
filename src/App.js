71196d7f6a25c52e887e56ecc7dae3ad65839898
deentendulkar <deenwaran6@gmail.com>
2019-12-03 17:07:54 +0530

    did

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  return (
+
+                <div className="container">
+                       <div className="aws-logo">
+                         <img src="aws-logo-icon-PNG-Transparent-Background.png" alt="aws-logo" title="aws-logo"/>
+                       </div>
+
+                             <div className="vl"></div>
+
+                             <div className="content">
+                                   <h1>AWS Cloud Practitioner</h1>
+                                   <p>Certification Training</p>
+                                <div className="feture"><p className="fetu">Features</p>
+                                   <p>Cloud concept Introduction,<br></br>
+                                   AWS Core Services.</p>
+                                </div>
+                                <div className="rating">
+                                   <img src="star.svg" alt="star"/><p id="five">5.0</p>|<p>3 Modules</p>|<p>2 Live Project</p>
+                                </div>
+                             </div>
+                </div>
+
+          
+    
+
+  );
+}
+
+export default App;
